18b9e1d85440ed2798c26e05be8ab54d22794e44
amankumar6 <amankumar786dpsdh@gmail.com>
2021-10-14 11:20:26 +0530

    feat: inter

---
+++ temp.js
@@ -0,0 +1,42 @@
+let experience = [
+    {
+        title: "Three Arrow Technologies",
+        profile: "Jr. ReactJS Developer",
+        link: "https://threearrow.in/",
+        img: "https://d2fedaqd9kwzub.cloudfront.net/website/6cc558062ae69ea699d57c2b62ed8fd9.svg",
+        disc: "We design a road map towards the success of your Business in this Digital World.",
+        details: [
+            "Gained hands-on experience in ReactJS.",
+            "Tools Used: CSS, BootStrap and ReactJS",
+            "Accomplished: Learned new thing in ReactJS and enhance my knowledge in Web Development.",
+        ],
+        time: "Sep 2020 - Nov 2020 ",
+    },
+    {
+        title: "KoiReader",
+        profile: "Front-End Developer",
+        link: "https://www.koireader.com/",
+        img: "https://d2fedaqd9kwzub.cloudfront.net/website/6cc558062ae69ea699d57c2b62ed8fd9.svg",
+        disc: "Artificial Intelligence based document automation and video feed data capture suite of cognitive APIs designed.",
+        details: [
+            "Gained hands-on experience in web development, and maintaining internal web applications",
+            "Assisting the technical team with the design, development, and fixing of bugs in the website",
+            "Tools Used: PUG, CSS, Materialize, JavaScript, jQuery, MJML, Web Components, LitElement and Vaadin",
+            "Accomplished: Learned new thing in JavaScript and CSS also enhance my knowledge in Web Development.",
+        ],
+        time: "Sep 2020 - Nov 2020 ",
+    },
+    {
+        title: "ItWali",
+        profile: "Front-End Developer",
+        link: "https://itwali.com/",
+        img: "https://res.cloudinary.com/dbvthtwhc/image/upload/v1626705622/portfolio/itwali.png",
+        disc: "Committed To Helping Our Clients Succeed All You Need A Reliable source That Helps in dealing With Your Income Tax Planning.",
+        details: [
+            "During the Internship, I was assigned as a front end developer of Itwali.com. In that project, I was assigned to developed pages of Itwali.com.",
+            "Tools Used: HTML, CSS, BootStrap, JavaScript and jQuery",
+            "Accomplished: Learned many new library, increased my speed in UI development and enhance my knowledge in Web Development.",
+        ],
+        time: "Aug 2020 - Sept 2020",
+    },
+];
